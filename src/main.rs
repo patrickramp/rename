c7df82f0628ea2ca842ee5918e9a097cad4de934
Patrick <patrick@tutamail.com>
2023-08-26 21:11:34 -0400

    Cleaned up codebase and improved regex efficiency.

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,30 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // Get the current working directory.
     if let Ok(current_dir) = env::current_dir() {
         println!("Current working directory: {}", current_dir.display());
 
-        // Process the files in the current directory.
         if let Ok(entries) = fs::read_dir(&current_dir) {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    let old_path = entry.path();
-                    if let Some(old_name) = old_path.file_name() {
-                        if let Some(old_str) = old_name.to_str() {
-                            // Check if the file name contains "S01E01" or similar using regex
-                            if let Some(new_name) = parse_file_name(old_str) {
-                                // Preserve the original file extension (if it exists)
-                                let extension = old_path.extension();
-                                let new_filename = if let Some(ext) = extension {
-                                    format!("{}.{}", new_name, ext.to_string_lossy())
-                                } else {
-                                    new_name.to_string()
-                                };
+            let regex = Regex::new(r"(?i)(.*S\d+E\d+)").unwrap();
 
-                                // Create the new path in the current directory.
-                                let new_path = current_dir.join(new_filename);
+            for entry in entries.flatten() {
+                let old_name = entry.file_name();
+                if let Some(old_str) = old_name.to_str() {
+                    if let Some(new_name) = parse_file_name(&regex, old_str) {
+                        let extension = entry.path().extension().map(|ext| ext.to_string_lossy().into_owned());
 
-                                // Rename the file and output changed files.
-                                if let Err(e) = fs::rename(&old_path, &new_path) {
-                                    eprintln!("Error renaming file: {:?}", e);
-                                } else {
-                                    println!(
-                                        "Renamed: {} -> {}",
-                                        old_path.display(),
-                                        new_path.display()
-                                    );
-                                }
-                            }
+                        let new_filename = if let Some(ext) = extension.as_ref() {
+                            format!("{}.{}", new_name, ext)
+                        } else {
+                            new_name.to_string()
+                        };
+
+                        let new_path = current_dir.join(new_filename);
+
+                        if let Err(e) = fs::rename(&entry.path(), &new_path) {
+                            eprintln!("Error renaming file: {:?}", e);
+                        } else {
+                            println!("Renamed: {} -> {}", entry.path().display(), new_path.display());
                         }
                     }
                 }
@@ -48,12 +37,6 @@ fn main() {
     }
 }
 
-// Parse the original filename and trim after "S01E01" or similar
-fn parse_file_name(filename: &str) -> Option<String> {
-    let regex = Regex::new(r"(?i)(.*S\d+E\d+)").unwrap();
-    if let Some(captures) = regex.captures(filename) {
-        Some(captures.get(1).unwrap().as_str().to_string())
-    } else {
-        None
-    }
+fn parse_file_name(regex: &Regex, filename: &str) -> Option<String> {
+    regex.captures(filename).map(|captures| captures[1].to_string())
 }
